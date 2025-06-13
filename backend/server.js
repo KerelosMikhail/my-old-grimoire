b1ed2ca63615ab890ef6d19762fcc77d9479b10c
Kerelos Mikhail <Kerelos.Adel@gmail.com>
2025-06-13 12:22:38 -0400

    Updating app.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,48 @@
-// Description: A simple Node.js server that listens on a specified port and responds with a message.
-// Usage: Run this script using Node.js to start the server.
-
-//console.log("This is my node app!");
-
 const http = require("http");
-const server = http.createServer((req, res) => {
-  res.end("This is my server 3rd response!");
+const app = require("./app");
+
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? "pipe " + address : "port: " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+server.on("error", errorHandler);
+server.on("listening", () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+  console.log("Listening on " + bind);
 });
-server.listen(process.env.PORT || 3000);
+
+server.listen(port);
