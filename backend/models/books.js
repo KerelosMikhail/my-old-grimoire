e6aa609fce5e586595b463d06d4bff69e21afd45
Kerelos Mikhail <Kerelos.Adel@gmail.com>
2025-06-16 20:20:20 -0400

    Adding models and book.js

---
+++ backend/models/books.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const bookSchema = mongoose.Schema({
+  userId: { type: String, required: true },
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  year: { type: Number, required: true },
+  genre: { type: String, required: true },
+  ratings: [
+    {
+      userId: { type: String, required: true },
+      grade: { type: Number, required: true, min: 0, max: 5 },
+    },
+  ],
+
+  averageRating: { type: Number, default: 0 },
+});
+
+module.exports = mongoose.model("Book", bookSchema);
+
+/*
+    * Book model
+    * Represents a book in the database.
+    * {
+  "id": "1",
+  "userId" : "clc4wj5lh3gyi0ak4eq4n8syr",
+  "title" : "Milwaukee Mission",
+  "author": "Elder Cooper",
+  "imageUrl" : "https://via.placeholder.com/206x260",
+  "year" : 2021,
+  "genre" : "Policier",
+  "ratings" : [{
+        "userId" : "1",
+        "grade": 5
+    },
+    {
+      "userId" : "1",
+      "grade": 5
+    },
+    {
+      "userId" : "clc4wj5lh3gyi0ak4eq4n8syr",
+      "grade": 5
+    },
+    {
+      "userId" : "1",
+      "grade": 5
+    }],
+  "averageRating": 3
+},
+    */
