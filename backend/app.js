8ea88e187d47b3863cb501ccf1e43c74a404cdab
Kerelos Mikhail <Kerelos.Adel@gmail.com>
2025-06-20 04:26:27 -0400

    Adding bcrypt, login, signup

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const app = express();
 const uri = process.env.MONGODB_URI;
 
 const stuffRoutes = require("./routes/stuff");
+const userRoutes = require("./routes/user");
 
 // Connect to MongoDB Atlas
 mongoose
@@ -35,33 +36,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// TODO
-// /api/auth/signup
-app.post("/api/auth/signup", (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: "Sign up successfully",
-  });
-});
-
-// TODO
-// /api/auth/login
-app.post("/api/auth/login", (req, res, next) => {
-  // Simulating a login
-  const { email, password } = req.body;
-
-  if (email === "" || password === "") {
-    return res.status(400).json({
-      message: "Email and password are required",
-    });
-  }
-  // Simulating a successful login
-  res.status(200).json({
-    message: "Login successful",
-    userId: 1, // Simulated user ID
-    token: "fake-jwt-token", // Simulated JWT token
-  });
-});
+app.post("/api/auth", userRoutes);
 
 app.use("/api/books", stuffRoutes);
 
